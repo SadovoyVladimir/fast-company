eb60b7255c7c83559104eddd6c577621608e16af
Vova <vlasadovoj@gmail.com>
2022-12-24 17:04:05 +0300

    Изменены отступы, подредактирован код

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Users from './components/users'
 import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
-		<Users />
-	</React.StrictMode>
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
